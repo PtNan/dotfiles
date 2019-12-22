6285842fd794689a59af2ccc0433de97db753da5
PtNan <imasxu@gmail.com>
2019-12-22 16:11:40 +0800

    update

--- profile.sh
+++ profile.sh
@@ -5,12 +5,6 @@ xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #im-select
 curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
-# fish
-brew install fish
-# fisher
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-fisher add rafaelrinaldi/pure
-fisher add jethrokuan/z
 # vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -21,23 +15,15 @@ cp *.otf ~/Library/Fonts/
 
 # dotfile
 mkdir -p ~/.config/nvim
-mkdir -p ~/.config/fish
-cp -i .latexmkrc .clang-format ~/
 cp -i coc-settings.json init.vim  ~/.config/nvim
-cp -i config.fish ~/.config/fish
-
 
 # homebrew
-brew install clang-format
 brew install git
 brew install go
-brew install --HEAD neovim
+brew install neovim
 brew install node
-brew install llvm
-brew install p7zip
 brew install ripgrep
 brew install unrar
-brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 brew install yarn
 
 
@@ -46,13 +32,7 @@ brew cask install appcleaner
 brew cask install basictex
 brew cask install iina
 brew cask install iterm2
-brew cask install motrix
 brew cask install shadowsocksx-ng-r
-
-
-# basictex
-sudo tlmgr install latexmk
-sudo tlmgr install lstaddons
 
 # coc
 mkdir -p ~/.config/coc/extensions
@@ -63,4 +43,3 @@ then
 fi
 # Change arguments to extensions you need
 yarn add coc-explorer coc-git coc-go coc-highlight coc-json coc-lists coc-pairs coc-snippets coc-word coc-yank
-
