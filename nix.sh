42b92424a09d2aab9071d86824391e3d0bf3299d
xujingnan <xujingnan@bilibili.com>
2021-12-08 10:05:20 +0800

    update

--- nix.sh
+++ nix.sh
@@ -2,5 +2,8 @@ sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-v
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 sudo nix-channel --update
 nix-shell '<home-manager>' -A install
+ln -Fhnvs ~/dotfiles/nixpkgs ~/.config/nixpkgs
+home-manager switch
+ln -Fhnvs ~/dotfiles/nvim/lua ~/.config/nvim/lua
 nix-env -i go
 # /bin/bash -c "$(curl -fsSL http://kratos.bilibili.co/x/kratos/install.sh)"
